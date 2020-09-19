27eb3a3c4b6948fa2e61f126a12428227a9bbbce
yanamac <yana.mak89@gmail.com>
2020-09-19 12:48:25 +0200

    updated dependencies

--- dogMicroservice/src/main/java/com/udacity/lessonFour/dogMicroservice/repository/DogRepository.java
+++ dogMicroservice/src/main/java/com/udacity/lessonFour/dogMicroservice/repository/DogRepository.java
@@ -3,11 +3,11 @@ package com.udacity.lessonFour.dogMicroservice.repository;
 import com.udacity.lessonFour.dogMicroservice.entity.Dog;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@RepositoryRestResource
+@Repository
 public interface DogRepository extends CrudRepository<Dog, Long> {
     @Query("select d.id, d.breed from Dog d where d.id=:id")
     String findBreedById(Long id);
